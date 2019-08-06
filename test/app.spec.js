8d14ee3be1034606e38557a800c4e55e5da8f158
martin <prepxc@gmail.com>
2019-08-05 19:59:46 -0500

    Looks like our problem is not having babel

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,11 +1,13 @@
-/* global it */
+/* global describe, it, expect */
 
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../src/App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+describe('App.js', () => {
+  it('renders without crashing', () => {
+    const expected = 2
+    const result = 1 + 1
+    expect(result).toEqual(expected)
+  })
 })
