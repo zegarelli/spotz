b6dc72e2b6f2ece030eb743abba98e8843469785
Martin Zegarelli <prepxc@gmail.com>
2019-08-09 21:47:05 -0500

    fix: remove unneeded console.log

--- src/services/spots_service.js
+++ src/services/spots_service.js
@@ -2,7 +2,6 @@ async function getPlaces () {
   let resp
   try {
     resp = await window.fetch('http://localhost:9000/places/')
-    console.log('in service: ', resp.body)
     return await resp.json()
   } catch (err) {
     console.error(err)
