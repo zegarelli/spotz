e21144f51218a5c9a9635e0464e08ffbacb14127
martin <martinzegarelli@gmail.com>
2020-04-06 21:22:57 -0500

    fix: remove testing items

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -3,25 +3,6 @@ import { Loader, Item } from 'semantic-ui-react'
 import useDataFetch from '../hooks/fetchData'
 import { activitiesToItems } from '../utils/maps'
 
-const items = [
-  {
-    childKey: 0,
-    image: '/images/wireframe/image.png',
-    header: 'Header',
-    description: 'Description',
-    meta: 'Metadata',
-    extra: 'Extra'
-  },
-  {
-    childKey: 1,
-    image: '/images/wireframe/image.png',
-    header: 'Header',
-    description: 'Description',
-    meta: 'Metadata',
-    extra: 'Extra'
-  }
-]
-
 function Activities () {
   const [{ apiResult: activities, isLoading, isError }] = useDataFetch('http://localhost:9000/activities')
 
