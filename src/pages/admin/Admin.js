1f37a915558badac236f829bdb92cc0788a4b8c3
martin <martinzegarelli@gmail.com>
2020-04-23 00:09:35 -0500

    wip: adminHome, AdminUsers, EditUserScopes pages

--- src/pages/admin/Admin.js
+++ src/pages/admin/Admin.js
@@ -1,51 +1,12 @@
 import React from 'react'
-import { Header, Container, Loader, Table } from 'semantic-ui-react'
-import useDataFetch from '../../hooks/fetchData'
-import getSessionCookie from '../../common/session'
+import { Header } from 'semantic-ui-react'
 
-function EditActivity () {
-  const [{
-    apiResult: users,
-    isLoading: usersLoading,
-    isError: usersError
-  }] = useDataFetch('http://localhost:9000/users')
-
+function Admin () {
   return (
-    <div className='PlaceDetail'>
-      <Container>
-        {usersError && <b>Error</b>}
-        {!usersError && usersLoading && <Loader active />}
-        {!usersError && !usersLoading && users &&
-          <>
-            <Header as='h1'>Users</Header>
-
-            <Table celled>
-              <Table.Header>
-                <Table.Row>
-                  <Table.HeaderCell>Created</Table.HeaderCell>
-                  <Table.HeaderCell>Username</Table.HeaderCell>
-                  <Table.HeaderCell>Email</Table.HeaderCell>
-                  <Table.HeaderCell>Scopes</Table.HeaderCell>
-                </Table.Row>
-              </Table.Header>
-
-              <Table.Body>
-                {users.slice().map((user) => {
-                  return (
-                    <Table.Row key={user.id}>
-                      <Table.Cell>{user.created_at.slice(0, 10)}</Table.Cell>
-                      <Table.Cell>{user.username}</Table.Cell>
-                      <Table.Cell>{user.email}</Table.Cell>
-                      <Table.Cell>Scopes</Table.Cell>
-                    </Table.Row>
-                  )
-                })}
-              </Table.Body>
-            </Table>
-          </>}
-      </Container>
+    <div className='Admin Home'>
+      <Header as='h1'>Admin Home</Header>
     </div>
   )
 }
 
-export default EditActivity
+export default Admin
---
+++ src/pages/admin/EditUserScopes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EditUserScopes () {
+  return (
+    <div className='PlaceDetail'>
+      <h1>UserDetail</h1>
+    </div>
+  )
+}
+
+export default EditUserScopes
