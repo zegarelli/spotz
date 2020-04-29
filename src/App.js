f2773c868bb6d4b739dc7868a8045792cfeb2d71
martin <martin.zegarelli@choosemylo.com>
2020-04-28 23:03:42 -0500

    chore: split app routes up into parts to handle login

--- src/App.js
+++ src/App.js
@@ -1,69 +1,20 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 
-// pages
-import Menu from './components/Menu'
-
-import Home from './pages/Home'
-
-import Places from './pages/Places'
-import CreatePlace from './pages/CreatePlace'
-import PlaceDetail from './pages/PlaceDetail'
-import EditPlace from './pages/EditPlace'
-
-import Activities from './pages/Activities'
-import ActivityDetail from './pages/ActivityDetail'
-import CreateActivity from './pages/CreateActivity'
-import EditActivity from './pages/EditActivity'
-
 import AdminRouter from './pages/admin/AdminRouter'
+import FrontRouter from './pages/frontRouter'
 
-// Hooks
-import useSession from './hooks/useSession'
+import AuthRedirect from './pages/AuthRedirect'
 
 function App () {
-  useSession()
 
   return (
     <div className='App' data-testid='app'>
       <BrowserRouter>
-        <Menu />
         <Switch>
           <Route
-            path='/places/new'
-            component={() => <CreatePlace />}
-          />
-          <Route
-            path='/places/:id/edit'
-            component={() => <EditPlace />}
-          />
-          <Route
-            path='/places/:id'
-            component={() => <PlaceDetail />}
-          />
-          <Route
-            path='/places'
-            component={() => <Places />}
-          />
-          <Route
-            path='/activities/new'
-            component={() => <CreateActivity />}
-          />
-          <Route
-            path='/activities/:id/edit'
-            component={() => <EditActivity />}
-          />
-          <Route
-            path='/activities/:id'
-            component={() => <ActivityDetail />}
-          />
-          <Route
-            path='/activities'
-            component={() => <Activities />}
-          />
-          <Route
-            path='/events'
-            component={() => <Places />}
+            path='/auth'
+            component={() => <AuthRedirect />}
           />
           <Route
             path='/admin'
@@ -71,7 +22,7 @@ function App () {
           />
           <Route
             path='/'
-            component={() => <Home />}
+            component={() => <FrontRouter />}
           />
           <Redirect to='/' />
         </Switch>
