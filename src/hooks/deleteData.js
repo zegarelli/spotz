7ffee1f556050c789d558d516609ac37df0c23ba
martin <martin.zegarelli@choosemylo.com>
2020-04-28 22:12:30 -0500

    feat: add delete hook

---
+++ src/hooks/deleteData.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react'
+
+const useDataDelete = (initialUrl, initialPayload) => {
+  const [url, setUrl] = useState(initialUrl)
+  const [payload, setPayload] = useState(initialPayload)
+  const [apiResult, setApiResult] = useState(undefined)
+  const [isLoading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false)
+      setIsLoading(!!payload)
+
+      if (payload) {
+        try {
+          const res = await window.fetch(url, {
+            method: 'DELETE',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify({
+              ...payload
+            }),
+            credentials: 'include'
+          })
+          if (!res.ok) {
+            setIsError(true)
+          }
+          let apiResult = url !== ''
+          if (apiResult) {
+            const contentType = res.headers.get('content-type')
+            const isJson =
+              contentType && contentType.indexOf('application/json') !== -1
+            apiResult = isJson ? await res.json() : { result: await res.text() }
+          }
+          setApiResult(apiResult)
+        } catch (err) {
+          setIsError(true)
+        } finally {
+          setIsLoading(false)
+        }
+      }
+    }
+
+    fetchData()
+  }, [payload, url])
+
+  return [{ apiResult, isLoading, isError }, setUrl, setPayload]
+}
+
+export default useDataDelete
