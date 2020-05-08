3f80118ee30ccd80ed4fe00e73364226baa686e6
martin <prepxc@gmail.com>
2020-05-08 18:52:03 -0500

    fix: failing useless unit tests (need to write more)

--- src/test/components/ActivityCard.spec.js
+++ src/test/components/ActivityCard.spec.js
@@ -2,34 +2,36 @@
 
 import React from 'react'
 import { render } from '@testing-library/react'
-import Activity from '../../components/Activity'
+import { BrowserRouter } from 'react-router-dom'
+import Activity from '../../components/ActivityCard'
 
 describe('Activity.js', () => {
   it('renders without crashing', () => {
-    const { getByTestId } = render(<Activity name='name' place_id={1} created_at='1234' />)
+    const { getByTestId } = render(
+      <BrowserRouter>
+        <Activity name='name' id='123' created_at='1234' />
+      </BrowserRouter>
+    )
     expect(getByTestId('activity')).toBeTruthy()
   })
   it('adds the header', () => {
-    const { getByText } = render(<Activity name='name' place_id={1} created_at='1234' />)
+    const { getByText } = render(
+      <BrowserRouter>
+        <Activity name='name' id='123' created_at='1234' />
+      </BrowserRouter>
+    )
     expect(getByText('name')).toBeTruthy()
-  })
-  it('adds the description', () => {
-    const name = 'Buzzard Beach'
-    const placeId = 12345
-    const { getByText } = render(<Activity name={name} place_id={placeId} created_at='1234' />)
-    expect(getByText(`${name} at place ${placeId}`)).toBeTruthy()
-  })
-  it('adds the extra', () => {
-    const created = '1234'
-    const { getByText } = render(<Activity name='name' place_id={1} created_at={created} />)
-    expect(getByText(`Created At: ${created}`)).toBeTruthy()
   })
   it('checks props', () => {
     const spy = jest.spyOn(console, 'error').mockReturnValue('')
-    render(<Activity created_at={123} />)
+    render(
+      <BrowserRouter>
+        <Activity created_at={123} />
+      </BrowserRouter>
+    )
     expect(spy).toHaveBeenCalledTimes(3)
-    expect(spy).toHaveBeenCalledWith(expect.stringMatching(/Failed prop type: The prop `name` is marked as required in `Activity`/))
-    expect(spy).toHaveBeenCalledWith(expect.stringMatching(/Failed prop type: The prop `place_id` is marked as required in `Activity`/))
-    expect(spy).toHaveBeenCalledWith(expect.stringMatching(/Failed prop type: Invalid prop `created_at` of type `number` supplied to `Activity`/))
+    expect(spy).toHaveBeenCalledWith(expect.stringMatching(/Failed prop type: The prop `name` is marked as required in `ActivityCard`/))
+    expect(spy).toHaveBeenCalledWith(expect.stringMatching(/Failed prop type: The prop `id` is marked as required in `ActivityCard`/))
+    expect(spy).toHaveBeenCalledWith(expect.stringMatching(/Failed prop type: Invalid prop `created_at` of type `number` supplied to `ActivityCard`/))
   })
 })
--- src/test/components/PlaceCard.spec.js
+++ src/test/components/PlaceCard.spec.js
@@ -2,31 +2,51 @@
 
 import React from 'react'
 import { render } from '@testing-library/react'
-import Place from '../../components/Place'
+import { BrowserRouter } from 'react-router-dom'
+import Place from '../../components/PlaceCard'
 
 describe('Place.js', () => {
   it('renders without crashing', () => {
-    const { getByTestId } = render(<Place name='name' extended_data={{ description: 'test' }} created_at='1234' />)
+    const { getByTestId } = render(
+      <BrowserRouter>
+        <Place id='1234' name='name' extended_data={{ description: 'test' }} created_at='1234' />
+      </BrowserRouter>
+    )
     expect(getByTestId('place')).toBeTruthy()
   })
   it('adds the header', () => {
-    const { getByText } = render(<Place name='name' extended_data={{ description: 'test' }} created_at='1234' />)
+    const { getByText } = render(
+      <BrowserRouter>
+        <Place id='1234' name='name' extended_data={{ description: 'test' }} created_at='1234' />
+      </BrowserRouter>
+    )
     expect(getByText('name')).toBeTruthy()
   })
   it('adds the description', () => {
-    const { getByText } = render(<Place name='name' extended_data={{ description: 'test' }} created_at='1234' />)
+    const { getByText } = render(
+      <BrowserRouter>
+        <Place id='1234' name='name' extended_data={{ description: 'test' }} created_at='1234' />
+      </BrowserRouter>
+    )
     expect(getByText('test')).toBeTruthy()
   })
   it('adds the meta', () => {
     const extendedData = { opens: 'test', closes: 'test' }
-    const { getByText } = render(<Place name='name' extended_data={extendedData} created_at='1234' />)
+    const { getByText } = render(
+      <BrowserRouter>
+        <Place id='1234' name='name' extended_data={extendedData} created_at='1234' />
+      </BrowserRouter>
+    )
     expect(getByText(`Opens: ${extendedData.opens} Closes: ${extendedData.closes}`)).toBeTruthy()
   })
   it('checks props', () => {
     const spy = jest.spyOn(console, 'error').mockReturnValue('')
-    render(<Place created_at={123} />)
+    render(
+      <BrowserRouter>
+        <Place id='1234' created_at={123} />
+      </BrowserRouter>)
     expect(spy).toHaveBeenCalledTimes(3)
-    expect(spy).toHaveBeenCalledWith(expect.stringMatching(/Failed prop type: The prop `name` is marked as required in `Place`/))
-    expect(spy).toHaveBeenCalledWith(expect.stringMatching(/Failed prop type: The prop `extended_data` is marked as required in `Place`/))
+    expect(spy).toHaveBeenCalledWith(expect.stringMatching(/Failed prop type: The prop `name` is marked as required in `PlaceCard`/))
+    expect(spy).toHaveBeenCalledWith(expect.stringMatching(/Failed prop type: The prop `extended_data` is marked as required in `PlaceCard`/))
   })
 })
