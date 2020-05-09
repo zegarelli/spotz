bcdb3f78e3446495c176686bc0b3f5581b2edcd3
martin <prepxc@gmail.com>
2020-05-09 01:17:15 -0500

    test: app.js routes correctly

--- src/test/app.spec.js
+++ src/test/app.spec.js
@@ -1,12 +1,61 @@
-/* global describe, it, expect */
+/* global describe, it, expect, afterEach, beforeEach, jest */
 
 import React from 'react'
 import { render } from '@testing-library/react'
 import App from '../App'
 
+import { MemoryRouter } from 'react-router'
+jest.mock('../routers/AdminRouter', () => {
+  const AdminRouter = () => <div data-testid='adminRouter' />
+  return AdminRouter
+})
+
+jest.mock('../routers/FrontRouter', () => {
+  const FrontRouter = () => <div data-testid='frontRouter' />
+  return FrontRouter
+})
+
+jest.mock('../pages/AuthRedirect', () => {
+  const AuthRedirect = () => <div data-testid='authRedirect' />
+  return AuthRedirect
+})
+
 describe('App.js', () => {
   it('renders without crashing', () => {
-    const { getByTestId } = render(<App />)
+    const { getByTestId } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>)
     expect(getByTestId('app')).toBeTruthy()
+  })
+  it('renders the FrontRouter', () => {
+    const { getByTestId } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>)
+    expect(getByTestId('frontRouter')).toBeTruthy()
+  })
+  it('renders the AdminRouter', () => {
+    const { getByTestId } = render(
+      <MemoryRouter initialEntries={['/admin']}>
+        <App />
+      </MemoryRouter>
+    )
+    expect(getByTestId('adminRouter')).toBeTruthy()
+  })
+  it('renders the AuthRedirect', () => {
+    const { getByTestId } = render(
+      <MemoryRouter initialEntries={['/auth']}>
+        <App />
+      </MemoryRouter>
+    )
+    expect(getByTestId('authRedirect')).toBeTruthy()
+  })
+  it('redirect all others to FrontRouter', () => {
+    const { getByTestId } = render(
+      <MemoryRouter initialEntries={['/fkjasdhfl']}>
+        <App />
+      </MemoryRouter>)
+    expect(getByTestId('frontRouter')).toBeTruthy()
   })
 })
