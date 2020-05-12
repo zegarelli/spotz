da3973eeb56b6f5ac14010d82c8482ef37d94164
martin <prepxc@gmail.com>
2020-05-11 21:44:50 -0500

    test: menu component tests

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -16,13 +16,17 @@ export default class MenuInst extends Component {
   }
 
   render() {
-    const { activeItem } = this.state
+    const { activeItem } = this.state    
+    const cognitoUrl = window.location.origin.toString() === 'http://localhost:3000' ?
+    'https://spotsauth.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=2uev36h1mbrgu1gtsfk57ai09m&redirect_uri=http://localhost:3000/auth' 
+    : 'https://spotz.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=32hvc3lpimluo1ro0bc8p0pjm8&redirect_uri=https://spotz.world/auth'
+
     return (
       <Menu data-testid='menu'>
-          <Menu.Item as={ Link } to='/' name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
-          <Menu.Item as={ Link } to='/places' name='places' active={activeItem === 'places'} onClick={this.handleItemClick} />
-          <Menu.Item as={ Link } to='/activities' name='activities' active={activeItem === 'activities'} onClick={this.handleItemClick}/>
-          <Menu.Item as={ Link } to='/places' name='events' active={activeItem === 'events'} onClick={this.handleItemClick} />
+          <Menu.Item as={ Link } to='/' name='home' id='menu-home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+          <Menu.Item as={ Link } to='/places' name='places' id='menu-places' active={activeItem === 'places'} onClick={this.handleItemClick} />
+          <Menu.Item as={ Link } to='/activities' name='activities' id='menu-activities' active={activeItem === 'activities'} onClick={this.handleItemClick}/>
+          <Menu.Item as={ Link } to='/places' name='events' id='menu-events' active={activeItem === 'events'} onClick={this.handleItemClick} />
         <Menu.Menu position='right'>
           <Menu.Item>
             <Input icon='search' placeholder='Search...' />
@@ -31,15 +35,17 @@ export default class MenuInst extends Component {
           !(this.session && this.session.username) ? 
           <Menu.Item
             as= 'a'
-            href={`https://spotz.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=32hvc3lpimluo1ro0bc8p0pjm8&redirect_uri=${window.location.origin.toString()}/auth`}
+            href={cognitoUrl}
             name='login'
+            id='menu-login'
           /> 
           : 
           <>
-            <Menu.Item as={ Link } to='/profile' name={this.session.username}/>
+            <Menu.Item as={ Link } to='/profile' id='menu-profile' name={this.session.username}/>
             <Menu.Item
             name='logout'
             onClick={this.handleLogout}
+            id='menu-logout'
             />
         </>
         }
--- src/test/components/Menu.spec.js
+++ src/test/components/Menu.spec.js
@@ -1,13 +1,50 @@
-/* global describe, it, expect */
+/* global describe, it, expect, jest, beforeEach */
 
 import React from 'react'
 import { render } from '@testing-library/react'
 import Menu from '../../components/Menu'
 import { MemoryRouter } from 'react-router-dom'
+import getSessionCookie from '../../common/session'
+
+jest.mock('../../common/session')
+
+beforeEach(function () {
+  getSessionCookie.mockImplementation(jest.fn(function () {
+    return { verified: false }
+  }))
+})
 
 describe('Menu.js', () => {
   it('renders without crashing', () => {
     const { getByTestId } = render(<MemoryRouter><Menu /></MemoryRouter>)
     expect(getByTestId('menu')).toBeTruthy()
+  })
+  it('has links to home, places, activities, and events', function () {
+    const { container } = render(<MemoryRouter><Menu /></MemoryRouter>)
+    const home = container.querySelector('#menu-home')
+    expect(home.pathname).toEqual('/')
+    const places = container.querySelector('#menu-places')
+    expect(places.pathname).toEqual('/places')
+    const activities = container.querySelector('#menu-activities')
+    expect(activities.pathname).toEqual('/activities')
+    const events = container.querySelector('#menu-events')
+    expect(events.pathname).toEqual('/places')
+  })
+  it('has a login button when session is not valid', function () {
+    const { container } = render(<MemoryRouter><Menu /></MemoryRouter>)
+    const login = container.querySelector('#menu-login')
+    expect(login).toBeTruthy()
+  })
+  it('has logout & profile buttons when the session is valid', function () {
+    getSessionCookie.mockImplementation(jest.fn(function () {
+      return { verified: true, username: 'martin' }
+    }))
+    const { container } = render(<MemoryRouter><Menu /></MemoryRouter>)
+    const logout = container.querySelector('#menu-logout')
+    expect(logout).toBeTruthy()
+    const profile = container.querySelector('#menu-profile')
+    expect(profile).toBeTruthy()
+    expect(profile.text).toEqual('Martin')
+    expect(profile.pathname).toEqual('/profile')
   })
 })
